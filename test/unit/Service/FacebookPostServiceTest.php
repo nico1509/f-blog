eff8553b6b84bb85f1a8a746daa38e8cb7560ee4
Nico Aßfalg <nico.assfalg@student.kit.edu>
2020-08-26 17:25:37 +0200

    Update FacebookPostServiceTest

--- test/unit/Service/FacebookPostServiceTest.php
+++ test/unit/Service/FacebookPostServiceTest.php
@@ -1,5 +1,8 @@
 <?php
 
+namespace Nico1509\Facebookblog\Test\Unit\Service;
+
+use DateTime;
 use Nico1509\Facebookblog\Model\Log;
 use Nico1509\Facebookblog\Model\FacebookPost;
 use Nico1509\Facebookblog\Service\FacebookPostService;
@@ -10,47 +13,28 @@ class FacebookPostServiceTest extends TestCase {
     private FacebookPostService $facebookFeedService;
 
     public function testGetValidatedPostList(): void {
-//        $feedData = [
-//            'message'      => 'asidhasofihasoihfoaishfoi',
-//            'id'           => '100325135089197_100868891701488',
-//            'created_time' => '2020-07-05T10:31:50+0000',
-//            'attachements' => [
-//                'data' => [
-//                    [
-//                        'media'  => [
-//                            'image' => [
-//                                'src' => 'https',
-//                            ],
-//                        ],
-//                        'target' => [
-//                            'url' => 'abc',
-//                        ],
-//                        'type' => 'photo'
-//                    ],
-//                ],
-//            ],
-//        ];
-
         $pageFeedJson = file_get_contents(__DIR__ . '/../Fixture/page-feed-single-post.json');
-        // TODO: Alle Types testen
         $feedData = json_decode($pageFeedJson, true);
 
         $imagePostJson = file_get_contents(__DIR__ . '/../Fixture/post-attachement-image.json');
         $imagePostData = json_decode($imagePostJson, true);
 
-        foreach ( $feedData['data'] as &$post ) {
-            if ($post['id'] === $imagePostData['id']) {
-                $post = array_merge($post, $imagePostData);
+        foreach ( $feedData['data'] as &$postData ) {
+            if ($postData['id'] === $imagePostData['id']) {
+                $postData['attachments'] = $imagePostData['attachments'];
             }
         }
 
-        // TODO: Medien einbinden, gescheite Fixture anlegen
         $expectedPostList   = [];
         $expectedPostList[] = new FacebookPost(
             'asidhasofihasoihfoaishfoi',
             '100325135089197_100868891701488',
-            new \DateTime( '2020-07-05T10:31:50+0000' )
+            new DateTime( '2020-07-05T10:31:50+0000' ),
+            'https://scontent.ffra2-1.fna.fbcdn.net/v/t1.0-9/106790674_100868708368173_619122657616213457_n.png?_nc_cat=109&_nc_sid=8024bb&_nc_oc=AQl4JAIojNl_r5dcybHXR-MuxDlDyQ-7GsZWXjn-VLzBEue7cX1TsIc0Ic_V6cyhjScoK-pHAPZEb1FVKt7N-BNS&_nc_ht=scontent.ffra2-1.fna&oh=98c33be74d1841873c48127e799cae40&oe=5F28216D',
+            'https://www.facebook.com/100325135089197/photos/a.100868868368157/100868705034840/?type=3'
         );
+
+        // FIXME: Attachments nicht erkannt
 
         $postList = $this->facebookFeedService->getValidatedPostList( $feedData );
         self::assertEquals( $expectedPostList, $postList);
